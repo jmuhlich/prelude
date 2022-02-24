20c75b298e785ebd88ec93fdfb56aa40b0ac5baa
Jeremy Muhlich <jmuhlich@bitflood.org>
2022-02-24 16:19:54 -0500

    Local customizations

---
+++ personal/custom.el
@@ -0,0 +1,43 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-safe-themes
+   '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5"
+     default))
+ '(package-selected-packages
+   '(0x0 ace-window ag anaconda-mode anzu async avy beacon
+         browse-kill-ring company company-anaconda company-lsp counsel
+         crux csv-mode cython-mode dash dash-functional diff-hl
+         diminish discover-my-major dockerfile-mode easy-kill
+         editorconfig elisp-slime-nav epl exec-path-from-shell
+         expand-region f flycheck gh gist git-commit git-timemachine
+         gitconfig-mode gitignore-mode groovy-mode guru-mode helm
+         helm-projectile hl-todo ht imenu-anywhere ivy js2-mode
+         json-mode json-snatcher logito lsp-mode lsp-ui magit
+         markdown-mode marshal move-text nlinum operate-on-number
+         pcache projectile pythonic rainbow-delimiters rainbow-mode
+         ripgrep rjsx-mode rust-mode s scss-mode smartparens smartrep
+         solarized-theme spinner super-save swiper transient undo-tree
+         volatile-highlights web-mode which-key with-editor yaml-mode
+         zenburn-theme zop-to-char))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(prelude-auto-save nil)
+ '(prelude-guru nil)
+ '(safe-local-variable-values
+   '((whitespace-mode . 1) (whitespace-mode) (prelude-whitespace . t)
+     (prelude-whitespace)))
+ '(split-height-threshold nil)
+ '(super-save-mode nil)
+ '(tramp-use-connection-share nil)
+ '(whitespace-style '(face trailing tabs empty)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(global-nlinum-mode 1)
---
+++ personal/preload/main.el
@@ -0,0 +1,4 @@
+(setq prelude-theme 'solarized-dark-high-contrast)
+(setq prelude-minimalistic-ui t)
+(setq prelude-flyspell nil)
+(setq prelude-clean-whitespace-on-save nil)
